c0d1d5191d3b1a2e1d5277f5f589e48ee3bbf098
kayo-b <kyo.bhz@gmail.com>
2022-11-28 22:46:38 +0100

    basic slider structure finished

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 class Visibility {
 
-
-    // constructor(elementID) {
-    //     this.elementID = elementID;
-    // }
-
     hideDisplay(element) {
         element.style.display = "none";
     }
@@ -19,7 +14,7 @@ class Visibility {
 
     eventListener(clickedElement, alterElement) {
         this.getElement(clickedElement).addEventListener("click", () => {
-
+            
         if(this.getElement(alterElement).style.display == "block"){
             let alterElementReturn = this.getElement(alterElement)
             this.hideDisplay(alterElementReturn)
@@ -31,16 +26,53 @@ class Visibility {
         });
     };
 
-    showSlides(index) {
+
+    eventListenerSlides(clickedElement) {
+        let element = this.getElement(clickedElement);
+        let slides = this.getElement("slides");
+        element.addEventListener("click", () => {
+        if(this.getElement(clickedElement).id == "arrowRight"){
+            for(let y = 0; y < slides.children.length; y++){
+                if(slides.children[y].style.display == "block"){
+                    if(y >= slides.children.length -1){
+                        console.log(y)
+                         this.showSlides(slides.children.length -1)    
+                }
+                    else{
+                        console.log(y)
+                        this.showSlides(y+1)
+                 }
+                    return
+                }
+            }
+        }
+
+        else {
+            for(let x = 0; x < slides.children.length; x++){
+                if(slides.children[x].style.display == "block"){
+                    if(x <= 0){
+                        this.showSlides(0)
+                    }
+                    else {
+                        this.showSlides(x-1)
+                    }
+                }      
+            }
+        }
+        });
+    };
+
+    showSlides(position) {
           let eleHTML = this.getElement("slides");
-          eleHTML.children[index].style.display = "block";
-
+          for(let x = 0; x < eleHTML.children.length; x++){
+            eleHTML.children[x].style.display = "none";
+        }   
+          eleHTML.children[position].style = `
+          display: block; `
     }
-
 }
 
 let visiTeste = new Visibility();
 visiTeste.eventListener("drop", "dropdown-content");
-visiTeste.eventListener("arrowLeft", "dropdown-content");
-visiTeste.eventListener("arrowRight", "dropdown-content");
-visiTeste.showSlides();
+visiTeste.eventListenerSlides("arrowLeft", "dropdown-content");
+visiTeste.eventListenerSlides("arrowRight", "dropdown-content");
